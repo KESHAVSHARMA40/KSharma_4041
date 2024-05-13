f071031cfbc91ad6be6124eae19efcc3a51a493c
Keshav Sharma <136235415+KESHAVSHARMA40@users.noreply.github.com>
2024-05-13 19:22:15 -0400

    Week1 Introduction to the EF

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Week1EFIntroduction
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {           
+
+        BookDBEntities db = new BookDBEntities();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            var books = db.Books.ToList();
+            grdBooks.ItemsSource = books;
+
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            Book newBook = new Book();
+            newBook.Title = txtTitle.Text;
+            newBook.Price = decimal.Parse(txtPrice.Text);
+
+            db.Books.Add(newBook);
+            db.SaveChanges();
+
+            txtTitle.Text = "";
+            txtPrice.Text = "";
+
+            var books = db.Books.ToList();
+            grdBooks.ItemsSource = books;
+
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,76 @@
+﻿SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Books](
+	[BookId] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](100) NULL,
+	[Price] [decimal](18, 2) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[BookId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 1/23/2024 12:51:50 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
+	[CustomerId] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](100) NULL,
+	[Address] [nvarchar](100) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Orders]    Script Date: 1/23/2024 12:51:50 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Orders](
+	[OrderId] [int] IDENTITY(1,1) NOT NULL,
+	[CustomerId] [int] NULL,
+	[BookId] [int] NULL,
+	[OrderDate] [date] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[OrderId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[Books] ON 
+
+INSERT [dbo].[Books] ([BookId], [Title], [Price]) VALUES (1, N'Learn C#', CAST(50.00 AS Decimal(18, 2)))
+INSERT [dbo].[Books] ([BookId], [Title], [Price]) VALUES (2, N'Database', CAST(30.00 AS Decimal(18, 2)))
+INSERT [dbo].[Books] ([BookId], [Title], [Price]) VALUES (3, N'HTML', CAST(25.00 AS Decimal(18, 2)))
+SET IDENTITY_INSERT [dbo].[Books] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Customers] ON 
+
+INSERT [dbo].[Customers] ([CustomerId], [Name], [Address]) VALUES (1, N'John', N'Toronto')
+INSERT [dbo].[Customers] ([CustomerId], [Name], [Address]) VALUES (2, N'Will', N'Montreal')
+INSERT [dbo].[Customers] ([CustomerId], [Name], [Address]) VALUES (3, N'Sara', N'Paris')
+SET IDENTITY_INSERT [dbo].[Customers] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Orders] ON 
+
+INSERT [dbo].[Orders] ([OrderId], [CustomerId], [BookId], [OrderDate]) VALUES (1, 1, 1, CAST(N'2024-01-16' AS Date))
+INSERT [dbo].[Orders] ([OrderId], [CustomerId], [BookId], [OrderDate]) VALUES (3, 1, 2, CAST(N'2024-01-16' AS Date))
+INSERT [dbo].[Orders] ([OrderId], [CustomerId], [BookId], [OrderDate]) VALUES (5, 3, 2, CAST(N'2024-01-16' AS Date))
+SET IDENTITY_INSERT [dbo].[Orders] OFF
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Books] FOREIGN KEY([BookId])
+REFERENCES [dbo].[Books] ([BookId])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Books]
+GO
+ALTER TABLE [dbo].[Orders]  WITH CHECK ADD  CONSTRAINT [FK_Orders_Customers] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([CustomerId])
+GO
+ALTER TABLE [dbo].[Orders] CHECK CONSTRAINT [FK_Orders_Customers]
+GO
